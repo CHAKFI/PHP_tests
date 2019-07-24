4b690c820ac75eab6c4b787f55adced763af57f9
CHAKFI <ahmedchakfi1999@gmail.com>
2019-07-24 12:15:14 +0100

    Update Person.php

--- POO_bhb/Person.php
+++ POO_bhb/Person.php
@@ -6,7 +6,42 @@
 			</head>
 			<body>
                 
-                
+                       <?php
+                     //define a class PERSON who content information of a person 
+                 class Person
+                 {
+                   $fname;
+                    $lname;
+                     $age;
+
+                  public : 
+                    Person();         //default constructor 
+                    Person($, $, $);    //initialization constructor
+                    Person(& );     //copy constructor
+                 }
+                     //definition of default constructor
+                      Person::Person()
+                      {
+                       echo "\r\nWELCOME TO DEFAULT CONSTRUCTOR\r\n";
+                      }
+
+                     //definition of initialization constructor
+                      Person::Person($a, $b, $c)
+                      {
+                      	echo "\r\nWELCOME TO INITIALIZATION CONSTRUCTOR\r\n";
+                         $fname = $a;
+                          $lname = $b;
+                       	   $age = $c;
+                      }
+
+                     //definition of copy constructor 
+                      Person::Person( &$ob)
+                      {
+                       echo "\r\nWELCOME TO COPY CONSTRUCTOR\r\n";
+                        
+                      }
+
+               ?>        
 
 			</body>
 			</html>
