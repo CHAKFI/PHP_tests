0450c74e324389ea2bd1d0bc59fd4e24b02095ae
CHAKFI <ahmedchakfi1999@gmail.com>
2019-07-31 15:20:37 +0100

    upd

--- PHP & MySQL/connect.php
+++ PHP & MySQL/connect.php
@@ -3,29 +3,37 @@
 		<head>
 			<title> BDD </title>
 		</head>
-		<body>
+		 <body align = "center" style="background-image:url('IMG/bg_img4.jpg')">
 		    
 		  <?php
-		$servername = "localhost";
-		$username = "root";
-		$password = "root";
+				$servername = "localhost";
+				$username = "root";
+				$password = "root";
 
-		// Create connection
-		$conn = mysqli_connect($servername, $username, $password);
+				// Create connection
+				$conn = mysqli_connect($servername, $username, );
 
-		// Check connection
-		if (!$conn) {
-		    die("Connection failed: " . mysqli_connect_error());
-		}
-		echo "Connected successfully";
-        
-        //create database
-        $sql = "CREATE DATABASE";
-          
+				// Check connection
+				if (!$conn)
+				   {
+				    die("Connection failed: " . mysqli_connect_error());
+				   }
+				    echo "Connection successfully\r\n";
+
+				
+                    // using database
+				   $usingDB = "USE person;";
+				if (mysqli_query($conn,$usingDB))
+				   {
+                    echo"Database changed and can be used";
+				   }
+				else
+				   {
+				   	echo "\r\nCan't using database : ".mysqli_error($conn)."\r\n";
+				   }   
 
 
-
-		?>
+		    ?>
 
 
 
