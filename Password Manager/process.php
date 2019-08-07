1d6aceab8125a8c8d4289e17bc40567156cacfea
CHAKFI <ahmedchakfi1999@gmail.com>
2019-08-07 11:39:55 +0100

    Update process.php

--- Password Manager/process.php
+++ Password Manager/process.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
+     
      <head>
-		<link rel="icon" type="image/png" href="img/icon.png">
+		    <link rel="icon" type="image/png" href="img/icon.png">
 			<title>~ Acceuil ~</title>
 	 </head>
+		
 		<body align="center" style="background-image:url('img/8.jpg'); background-size: cover;">
 		  <?php 
 		 
 		 // to get values passe from form in login.php file
-
 		     
 		     if(isset($_POST['user']))
 		       {
@@ -21,20 +22,22 @@
 		  	    $password = $_POST['pass'];
 		       }
 		              $servername = "localhost";
-						$username = "root";
-						$password = "";
+						$userSer = "root";
+						$pwdSer = "";
                         $dbname = "passman";
 		                 //connect to the server select database
-		  				$con = mysqli_connect($servername, $username, $password);
+		  				$con = mysqli_connect($servername, $userSer, $pwdSer);
 						 mysqli_select_db($con, $dbname);
 
-				 // to prevent mysql injection
+		 // to prevent mysql injection
+
 					 $username = stripcslashes($username);
 					  $password = stripcslashes($password);
 					   $username = mysqli_real_escape_string($con, $username);
 						$password = mysqli_real_escape_string($con, $username);
 						
-						 // Query the database for user
+		 // Query the database for user
+
 						 $Rsql = "select * from authentf where username ='$username' and pssw ='$password'";
 					     $result = mysqli_query($con, $Rsql) or die('Failed to query database '.mysqli_error($con));
 					     $row = mysqli_fetch_array($result);
@@ -43,12 +46,16 @@
 					      {
 					    echo '<p style="font-family: Century Gothic; font-size: 30px; margin-left: 40%; color: white; width: 30%; background: rgba(1, 254, 18, 0.2); padding: 1px; border-radius: 15px;" login success! Welcome'.$row['username'].'</p>';
 		        	      } 
-		        	      
+
 		        	  else 
 		        	      {
-				        echo '<p style="font-family: Century Gothic; font-size: 30px; margin-left: 36%; color:white; width: 30%; background: rgba(254, 1, 1, 0.2); padding: 1px; border-radius: 15px;">Failed to login!<a href="login.php" style="font-family: Century Gothic; color:white;"> Try again</a></p>';
+				        echo '<p style="font-family: Century  Gothic; font-size: 30px; margin-left: 36%; color:white; width: 30%; background: rgba(254, 1, 1, 0.2); padding: 1px; border-radius: 15px;">Failed to login!<a href="login.php" style="font-family: Century Gothic; color:white;"> Try again</a></p>';
 		                  }
-		?>
+                           
+                       
+
+
+		?>               
 		</body>
 </html>
 
